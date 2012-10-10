ce117ffaf69a2af373b43258e795121abbc5522f
Steve Derico <steve@stevederico.com>
2012-10-10 00:26:13 -0700

    Added Guard and Spork

--- Gemfile
+++ Gemfile
@@ -8,8 +8,18 @@ gem 'heroku'
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
+  gem 'guard-rspec', '1.2.1'
+  gem 'guard-spork', '1.2.0'
+  gem 'spork', '0.9.2'
 end
 
+
+# Test gems on Macintosh OS X
+group :test do
+  gem 'capybara', '1.1.2'
+  gem 'rb-fsevent', '0.9.1', :require => false
+  gem 'growl', '1.0.3'
+end 
 
 # Gems used only for assets and not required
 # in production environments by default.
@@ -20,10 +30,6 @@ group :assets do
 end
 
 gem 'jquery-rails'
-
-group :test do
-  gem 'capybara', '1.1.2'
-end
 
 group :production do
   gem 'pg', '0.12.2'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,22 +1,28 @@
 require 'spec_helper'
 
 
-
+ 
 describe "Static pages" do
   
   let(:rrtsa) { "Ruby on Rails Tutorial Sample App" } 
 
   describe "Home page" do
-    it "should have the title Home" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "#{rrtsa} | Home")
+                        :text => "Ruby on Rails Tutorial Sample App")
     end
     
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+
   end
   
   describe "Help page" do
@@ -31,6 +37,7 @@ describe "Static pages" do
         page.should have_selector('title',
                         :text => "Ruby on Rails Tutorial Sample App | Help")
       end
+
   end
   
   describe "About page" do
